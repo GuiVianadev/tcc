2f06ecbc3e84d2cc0dbdff2d717c3effbe9fbaaa
GuiVianaDev <guilhermevianna@gmail.com>
2025-10-10 23:27:06 -0300

    feat: add update and delete user to update repository

--- backend-tcc/src/repositories/users-repository.ts
+++ backend-tcc/src/repositories/users-repository.ts
@@ -7,7 +7,11 @@ export type UserRepository = {
   create(
     data: InferInsertModel<typeof users>
   ): Promise<InferInsertModel<typeof users>>;
-
+  updateUser(
+    id: string,
+    data: Partial<InferInsertModel<typeof users>>
+  ): Promise<InferInsertModel<typeof users>>;
   findByEmail(email: string): Promise<InferInsertModel<typeof users> | null>;
   findById(id: string): Promise<InferSelectModel<typeof users> | null>;
+  deleteUser(id: string): Promise<boolean>;
 };
